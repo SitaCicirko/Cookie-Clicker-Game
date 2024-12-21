400eace80067760d346eb7b9092662d161cd6d82
Sita Cicirko <ns.cicirko@gmail.com>
2024-12-21 20:38:23 +0000

    2 commit

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const cookieButton = document.getElementById("cookieButton");
 const makeCookies = document.getElementById("cookieCount");
 
 let cookieCount = 0;
-let cookiesAdded = 1;
 
 cookieButton.addEventListener("click", function () {
-  cookieCount += cookiesAdded;
-  makeCookies.innerHTML = cookieCount;
+  cookieCount++;
+  makeCookies.innerHTML = "Cookies: " + cookieCount;
 });
