fc61fa05bf043efff2c5cae7dd9f3b67e0f737a2
Sita Cicirko <ns.cicirko@gmail.com>
2024-12-22 11:44:45 +0000

    3 commit

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const cookieButton = document.getElementById("cookieButton");
 const makeCookies = document.getElementById("cookieCount");
+const deleteCookies = document.getElementById("deleteCookies");
+const loadCookies = document.getElementById("loadCookies");
 
 let cookieCount = 0;
 
 cookieButton.addEventListener("click", function () {
   cookieCount++;
+  makeCookies.innerHTML = "Cookies: " + cookieCount;
+
+  const stringCookies = JSON.stringify(cookieCount);
+  localStorage.setItem("cookies", stringCookies);
+});
+
+loadCookies.addEventListener("click", function () {
+  const savedCookies = localStorage.getItem("cookies");
+
+  if (savedCookies) {
+    cookieCount = parseInt(savedCookies);
+    makeCookies.textContent = "Cookies: " + savedCookies;
+  } else {
+    makeCookies.innerHTML = "Cookies: 0";
+  }
+});
+
+deleteCookies.addEventListener("click", function () {
+  localStorage.removeItem("cookies");
+  cookieCount = 0;
   makeCookies.innerHTML = "Cookies: " + cookieCount;
 });
